0f7e8e2b5bd239405e3b24e8fc27294c1a0f7142
aydaruusmouse <halalmedia9@gmail.com>
2020-10-17 23:29:42 +0300

    insure.

---
+++ app.js
@@ -0,0 +1,5 @@
+const humberger= document.getElementById('humberger');
+const list = document.getElementById('list');
+humberger.addEventListener('click', ()=> {
+    list.classList.toggle('show');
+})
